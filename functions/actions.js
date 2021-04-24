1f86077905dfec652fb1dabe125d857a62b3d928
ljpmatsu <22matek@gmail.com>
2021-04-24 13:31:37 +0200

    simplifying save path

--- functions/actions.js
+++ functions/actions.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function saveAmount(user , userID) {
     const user1Json  = JSON.stringify(user);
-    fs.writeFileSync('./accounts/' + userID + '.json' , user1Json);
+    fs.writeFileSync(`./accounts/${userID}.json` , user1Json);
 }
 
 module.exports = { saveAmount };
